1b9091ceb0f7b15340b1ccccfb0072557434ee03
ChoiWonGyeom <cocomama333@naver.com>
2023-04-05 14:31:43 +0900

    라이브러리 설치

--- firstSolar/front/src/index.js
+++ firstSolar/front/src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+
+import awsconfig from "./aws-exports";
+
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={studioTheme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
