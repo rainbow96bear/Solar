9478033df205d590589f0fd3b7e34c23013d086a
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-04-07 17:47:51 +0900

    회의만함
    회의만함

--- firstSolar/back/src/routes/sync.ts
+++ firstSolar/back/src/routes/sync.ts
@@ -5,11 +5,9 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 router.post("/apiUse", async (req, res) => {
   try {
-    
   } catch (error) {
     console.error(error);
     res.send("실패");
   }
 });
 export default router;
-
