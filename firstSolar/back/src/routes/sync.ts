b548e80fd3186cf07645101feac60086b3065dc8
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-04-07 17:47:51 +0900

    회의만함
    회의만함

--- firstSolar/back/src/routes/sync.ts
+++ firstSolar/back/src/routes/sync.ts
@@ -35,18 +35,66 @@ router.post("/apiUse", async (req, res) => {
 });
 export default router;
 
-// import { BlockVisionProvider, BvNetwork } from "blockvision.js";
-
-// Optional parameters, but default to eth-mainnet and default api-key.
-// const bv = new BlockVisionProvider(BvNetwork.ETH_MAINNET);
-
-// Access standard Ethers.js JSON-RPC node request
-// bv.getBalance('0x407d73d8a49eeb85d32cf465507dd71d507100c1').then(console.log)
-
-// import { BlockVisionProvider, BvNetwork } from 'blockvision.js'
-
-// // Optional parameters, but default to eth-mainnet and default api-key.
-// const bv = new BlockVisionProvider(BvNetwork.POL_MAINNET)
-
-// // Access standard Ethers.js JSON-RPC node request
-// bv.getBalance('0x407d73d8a49eeb85d32cf465507dd71d507100c1').then(console.log)
+// --디파짓--
+// function deposit(uint _amount) public nonReentrant {
+//   strategy.beforeDeposit();
+//   uint256 _pool = balance();
+//   want().safeTransferFrom(msg.sender, address(this), _amount);
+//   earn();
+//   uint256 _after = balance();
+//   _amount = _after - _pool; // Additional check for deflationary tokens
+//   uint256 shares = 0;
+//   if (totalSupply() == 0) {
+//       shares = _amount;
+//   } else {
+//       shares = (_amount * totalSupply()) / _pool;
+//   }
+//   _mint(msg.sender, shares);
+// }
+//
+// --출금--
+// function withdraw(uint256 _shares) public {
+//   uint256 r = (balance() * _shares) / totalSupply();
+//   _burn(msg.sender, _shares);
+//   uint b = want().balanceOf(address(this));
+//   if (b < r) {
+//       uint _withdraw = r - b;
+//       strategy.withdraw(_withdraw);
+//       uint _after = want().balanceOf(address(this));
+//       uint _diff = _after - b;
+//       if (_diff < _withdraw) {
+//           r = b + _diff;
+//       }
+//   }
+//   want().safeTransfer(msg.sender, r);
+// }
+//
+// --earn--
+// function earn() public {
+//   uint _bal = available();
+//   want().safeTransfer(address(strategy), _bal);
+//   strategy.deposit();
+// }
+//
+// --proposeStrat--
+// function proposeStrat(address _implementation) public onlyOwner {
+//   require(address(this) == IStrategyV7(_implementation).vault(), "Proposal not valid for this Vault");
+//   require(want() == IStrategyV7(_implementation).want(), "Different want");
+//   stratCandidate = StratCandidate({
+//       implementation: _implementation,
+//       proposedTime: block.timestamp
+//   });
+//   emit NewStratCandidate(_implementation);
+// }
+//
+// --upgradeStrat--
+//  function upgradeStrat() public onlyOwner {
+//  require(stratCandidate.implementation != address(0), "There is no candidate");
+//  require(stratCandidate.proposedTime + approvalDelay < block.timestamp, "Delay has not passed");
+//  emit UpgradeStrat(stratCandidate.implementation);
+//  strategy.retireStrat();
+//  strategy = IStrategyV7(stratCandidate.implementation);
+//  stratCandidate.implementation = address(0);
+//  stratCandidate.proposedTime = 5000000000;
+//  earn();
+// }
