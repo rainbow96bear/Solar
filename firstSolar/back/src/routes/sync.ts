2d840e12c23567d3a9e190ee8d5ae8ccb3fbef93
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-04-15 15:14:02 +0900

    토요일 커밋
    토요일 커밋

--- firstSolar/back/src/routes/sync.ts
+++ firstSolar/back/src/routes/sync.ts
@@ -2,90 +2,79 @@ import express from "express";
 import axios from "axios";
 import Moralis from "moralis";
 import { EvmChain } from "@moralisweb3/common-evm-utils";
-
 import { mainNet } from "./defi";
 
 const router = express.Router();
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
-Moralis.start({
-  apiKey: "uYftfj0EOyThUZbG9TGDyuFa52N3Y9IS0177QWrIWTNI8wMWxJlyRGLCu7oQIS1f",
-});
+// Moralis.start({
+//   apiKey: "uYftfj0EOyThUZbG9TGDyuFa52N3Y9IS0177QWrIWTNI8wMWxJlyRGLCu7oQIS1f",
+// });
 
 router.get("/", (req, res) => {
   const runApp = async () => {
     try {
-      const activeLpList = (
-        await axios.get(`https://api.beefy.finance/vaults`)
-      ).data.filter(
-        (lp: any) => lp.status === "active" && lp.assets.length == 2
-      );
+      let coinListData = (
+        await axios.get("https://api.coingecko.com/api/v3/coins/list")
+      ).data;
+      let coinMarkets = (
+        await axios.get("https://api.coingecko.com/api/v3/coins/markets")
+      ).data;
+      res.send(coinListData);
+      // let lpsData = (await axios.get("https://api.beefy.finance/lps")).data;
 
-      const result = await axios.get(`https://api.beefy.finance/lps/breakdown`);
-      console.log(Object.keys(result));
-      console.log(result.data["curve-poly-usdr"]);
+      // let keyArray = Object.keys(lpsData);
+      // let valusArray = Object.values(lpsData);
+      // let indexArray = [];
+      // let filterArray = [];
+      // let fixArr = [];
 
-      try {
-        let response = await Moralis.EvmApi.token.getTokenPrice({
-          address: result.data["curve-poly-usdr"].tokens[0],
-          chain: 137,
-        });
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
+      // for (let i = 0; i < valusArray.length; i++) {
 
-      //   activeLpList.map(async(item, index)=>{
-      //     const network = item.network;
-      //   const tokenAddress = item.tokenAddress
-      //     let response = await Moralis.EvmApi.token.getTokenPrice({
-      //       address: tokenAddress,
-      //       chain : network,
-      //     });
-
-      //     console.log(response.toJSON());
-      // })
-
-      // for (let i = 0; i < activeLpList.length; i++) {
-      //   let address = tempAddressData[i];
-      //   let chain;
-
-      //   if (tempNetworkData[i] == "arbitrum") {
-      //     chain = EvmChain.ARBITRUM;
-      //   } else if (tempNetworkData[i] == "avalanche") {
-      //     chain = EvmChain.AVALANCHE;
-      //   } else if (tempNetworkData[i] == "bsc") {
-      //     chain = EvmChain.BSC;
-      //   } else if (tempNetworkData[i] == "cronos") {
-      //     chain = EvmChain.CRONOS;
-      //   } else if (tempNetworkData[i] == "ethereum") {
-      //     chain = EvmChain.ETHEREUM;
-      //   } else if (tempNetworkData[i] == "palm") {
-      //     chain = EvmChain.PALM;
-      //   } else if (tempNetworkData[i] == "sepolia") {
-      //     chain = EvmChain.SEPOLIA;
-      //   } else if (tempNetworkData[i] == "fantom") {
-      //     chain = EvmChain.FANTOM;
-      //   } else if (tempNetworkData[i] == "fuji") {
-      //     chain = EvmChain.FUJI;
-      //   } else {
-      //     chain = "";
-      //   }
-      //   // console.log(address);
-
-      //   try {
-      //     let response = await Moralis.EvmApi.token.getTokenPrice({
-      //       address,
-      //       chain,
-      //     });
-      //     // console.log(response);
-      //     price.push(response);
-      //   } catch (error) {
-      //     continue;
+      //   if ((valusArray[i] as number) > 1) {
+      //     indexArray.push(i);
+      //     filterArray.push(valusArray[i]);
       //   }
       // }
-      res.end();
+
+      // for (let i = 0; i < indexArray.length; i++) {
+      //   let filterKeyIndex = keyArray[indexArray[i]];
+
+      //   fixArr.push({ id: filterKeyIndex, price: filterArray[i] });
+      // }
+
+      // const activeLpList = (
+      //   await axios.get(`https://api.beefy.finance/vaults`)
+      // ).data.filter((lp: any) => lp.status === "active");
+
+      // const tempFinishData = [];
+      // for (let i = 0; i < fixArr.length; i++) {
+      //   const filterData = activeLpList.filter((lp) => lp.id == fixArr[i].id);
+      //   if (filterData.length) tempFinishData.push(...filterData);
+      // }
+
+      // let fantomData = tempFinishData.filter(
+      //   (lp) =>
+      //     lp.network == "fantom" ||
+      //     lp.network == "ethereum" ||
+      //     lp.network == "polygon" ||
+      //     lp.network == "bsc" ||
+      //     lp.network == "avalanche" ||
+      //     lp.network == "fuji" ||
+      //     lp.network == "cronos" ||
+      //     lp.network == "palm" ||
+      //     lp.network == "arbitrum"
+      // );
+
+      // const address = "0x9A8b2601760814019B7E6eE0052E25f1C623D1E6";
+      // const chain = EvmChain.POLYGON;
+      // const response = await Moralis.EvmApi.token.getTokenPrice({
+      //   address,
+      //   chain,
+      // });
+      // console.log(response.toJSON());
+      // response
     } catch (error) {
       console.log(error);
       res.end();
