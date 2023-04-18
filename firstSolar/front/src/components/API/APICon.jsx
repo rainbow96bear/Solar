1f5ab2ea1d3413ee9d0454e2311cd50ee31b4ac0
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-04-18 15:16:42 +0900

    Solar project 중간전체 머지 price값 받아 오는것 까지 확인

--- firstSolar/front/src/components/API/APICon.jsx
+++ firstSolar/front/src/components/API/APICon.jsx
@@ -3,38 +3,59 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const APICon = () => {
-  const [priceData, setPriceData] = useState([]);
+  const [NowUSDTprice, setNowUSDTprice] = useState([]);
+  const [NowETHprice, setNowETHprice] = useState([]);
+  const [NowBNBprice, setNowBNBprice] = useState([]);
+  const [BNBtoUSDT, setBNBtoUSDT] = useState([]);
+  const [BNBtoETH, setBNBtoETH] = useState([]);
+  const [ETHtoBNB, setETHtoBNB] = useState([]);
+  const [ETHtoUSDT, setETHtoUSDT] = useState([]);
+  const [USDTtoETH, setUSDTtoETH] = useState([]);
+  const [USDTtoBNB, setUSDTtoBNB] = useState([]);
 
   const APIClick = async () => {
     try {
-      let tempMoralisData = (await axios.get("http://localhost:8080/api/sync"))
-        .data;
+      const {
+        NowUSDTprice,
+        NowETHprice,
+        NowBNBprice,
+        BNBtoUSDT,
+        BNBtoETH,
+        ETHtoBNB,
+        ETHtoUSDT,
+        USDTtoETH,
+        USDTtoBNB,
+      } = (await axios.get("http://localhost:8080/api/sync")).data;
 
-      setPriceData(tempMoralisData);
+      setNowUSDTprice(NowUSDTprice);
+      setNowETHprice(NowETHprice);
+      setNowBNBprice(NowBNBprice);
+      setBNBtoUSDT(BNBtoUSDT);
+      setBNBtoETH(BNBtoETH);
+      setETHtoBNB(ETHtoBNB);
+      setETHtoUSDT(ETHtoUSDT);
+      setUSDTtoETH(USDTtoETH);
+      setUSDTtoBNB(USDTtoBNB);
     } catch (error) {
       console.log(error);
     }
   };
-  const APIClick2 = async () => {
-    try {
-      let lpsData = (await axios.get("https://api.beefy.finance/lps")).data;
-      let tokensData = (await axios.get("https://api.beefy.finance/tokens"))
-        .data;
-
-      console.log("lpsData", lpsData);
-
-      console.log("tokensData", tokensData);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  console.log(NowUSDTprice);
+  console.log(BNBtoETH);
 
   return (
     <div>
       <APIComp
         APIClick={APIClick}
-        APIClick2={APIClick2}
-        priceData={priceData}
+        NowUSDTprice={NowUSDTprice}
+        NowETHprice={NowETHprice}
+        NowBNBprice={NowBNBprice}
+        BNBtoUSDT={BNBtoUSDT}
+        BNBtoETH={BNBtoETH}
+        ETHtoBNB={ETHtoBNB}
+        ETHtoUSDT={ETHtoUSDT}
+        USDTtoETH={USDTtoETH}
+        USDTtoBNB={USDTtoBNB}
       />
     </div>
   );
