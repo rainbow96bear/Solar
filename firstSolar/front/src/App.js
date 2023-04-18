2d82a17b16fb02b7ccf7faa8f2ce18fe0bfc0a01
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2023-04-18 12:27:12 +0900

    230418 test dir delete

--- firstSolar/front/src/App.js
+++ firstSolar/front/src/App.js
@@ -1,11 +1,5 @@
-import DefiContainer from "./components/defiApi/Container";
-
 function App() {
-  return (
-    <div>
-      <DefiContainer />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
