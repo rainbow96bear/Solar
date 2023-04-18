046b7313c0796a6bc010d1d12c3a5000b5753ebc
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2023-04-18 15:12:31 +0900

    230418 back src merging

--- firstSolar/back/routes/index.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import user from "./user";
-const router = Router();
-router.use("/user", user);
-export default router;
--- firstSolar/back/src/routes/index.ts
+++ firstSolar/back/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import defi from "./defi";
+import user from "./user";
 
 const router = Router();
 
 router.use("/defi", defi);
+router.use("/user", user);
 
 export default router;
