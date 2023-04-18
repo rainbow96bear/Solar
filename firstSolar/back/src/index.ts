c49fa40ce25df2d244c234e6af968d61467c3933
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2023-04-18 10:49:52 +0900

    230418 rebase test

--- firstSolar/back/src/index.ts
+++ firstSolar/back/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./routes/index";
+import db from "../models/index";
 
 const app: Express = express();
 
@@ -11,14 +12,14 @@ app.use(cors({ origin: true, credential: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// db.sequelize
-//   .sync({ force: false })
-//   .then(() => {
-//     console.log("db connected");
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use("/api", routes);
 
