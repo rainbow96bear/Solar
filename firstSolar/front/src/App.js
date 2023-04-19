db3c7957ada38e2192762c4c87b2a315b6f260b3
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-04-19 15:11:50 +0900

    23.04.19
    23.04.19

--- firstSolar/front/src/App.js
+++ firstSolar/front/src/App.js
@@ -26,7 +26,7 @@ import { useWeb3Modal } from "@web3modal/react";
 import HeaderContainer from "./components/header/Container";
 import MainContainer from "./components/main/Container";
 import FooterContainer from "./components/footer/Container";
-import { Swap320px } from "./ui-components";
+import { Swap320px, Swap768px } from "./ui-components";
 import { connectThunk } from "./modules/connect";
 import UserLoading from "./ui-components/UserLoading";
 import SwapContainer from "./components/swap/Container";
