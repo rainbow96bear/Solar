a61a493eb8f40ae7141c7e1ceef94dc5a6a2232e
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-04-20 23:27:35 +0900

    04.20 swap approve transaction 완성
    04.20 swap approve transaction 완성

--- firstSolar/back/src/routes/index.ts
+++ firstSolar/back/src/routes/index.ts
@@ -3,13 +3,13 @@ import { Router } from "express";
 import defi from "./defi";
 import user from "./user";
 import sync from "./sync";
-import Dex from "./Dex";
+import swap from "./swap";
 
 const router = Router();
 
-router.use("/Dex", Dex);
 router.use("/defi", defi);
 router.use("/user", user);
 router.use("/sync", sync);
+router.use("/swap", swap);
 
 export default router;
