363dd1db8032e1d9ec724320cb6458d3bc9586ee
pippen11 <111038332+pippen11@users.noreply.github.com>
2023-04-20 20:25:39 +0900

    230420

--- firstSolar/back/contracts/IDFS.sol
+++ firstSolar/back/contracts/IDFS.sol
@@ -5,3 +5,6 @@ interface IDFS {
   function reward(address _to, uint256 _amount) external;
   //  function mint(address _to, uint256 _amount)external;
 }
+0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
+
