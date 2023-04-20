3519e9c96fe78bbe91825ccb24273b5d362fe16b
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2023-04-20 17:06:23 +0900

    230420 self pool staking back 설계

--- firstSolar/front/src/components/defiApi/Container.jsx
+++ firstSolar/front/src/components/defiApi/Container.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Web3 from "web3";
 
 import { mainNet, platform } from "../../mainNet";
 import DefiComponent from "./Component";
@@ -16,8 +15,6 @@ const DefiContainer = () => {
   const [status, setStatus] = useState(DATA_STATUS.WAIT);
   const [mainNetList, setMainNetList] = useState([]);
   const [dexList, setDexList] = useState([]);
-  const [inputValue, setInput] = useState("");
-  const [isAPI, setIsAPI] = useState("");
 
   const totalLpListUp = async () => {
     try {
@@ -57,57 +54,6 @@ const DefiContainer = () => {
     }
   };
 
-  const checkAPI = async (_inputValue) => {
-    try {
-      const result = (
-        await axios.post(`http://localhost:8080/api/defi/check`, {
-          inputAPI: _inputValue,
-        })
-      ).data;
-      setIsAPI(result.msg);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const createLiquidityPoolFunc = async () => {
-    try {
-      let web3;
-      if (window.ethereum) web3 = new Web3(window.ethereum);
-
-      const result = (
-        await axios.post("http://localhost:8080/api/defi/createLiquidityPool", {
-          account: "0xCB8775C5943Dc32ed8C1A8182Ce9A53E5b87383a",
-        })
-      ).data;
-      let transactionResult = await web3.eth.sendTransaction({
-        ...result,
-      });
-      console.log(result);
-      console.log(transactionResult);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getPool = async () => {
-    const info = await axios.post(
-      "http://localhost:8080/api/defi/insertLpToDB"
-    );
-    console.log(info);
-  };
-
-  const lpStaking = async () => {
-    try {
-      const result = (
-        await axios.post(`http://localhost:8080/api/defi/approve`)
-      ).data;
-      console.log(result);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   useEffect(() => {
     setMainNetList(Object.keys(mainNet));
     setDexList(Object.values(platform));
@@ -122,13 +68,6 @@ const DefiContainer = () => {
       mainNetList={mainNetList}
       dexList={dexList}
       filterNetworkAndDex={filterNetworkAndDex}
-      checkAPI={checkAPI}
-      isAPI={isAPI}
-      setIsAPI={setIsAPI}
-      inputValue={inputValue}
-      setInput={setInput}
-      createLiquidityPoolFunc={createLiquidityPoolFunc}
-      getPool={getPool}
     />
   );
 };
