0ff956423d91428590e990da1b7f5e1638183b0a
ChoiWonGyeom <cocomama333@naver.com>
2023-04-24 16:20:22 +0900

    cwgRebase

--- firstSolar/front/src/components/swap/Container.jsx
+++ firstSolar/front/src/components/swap/Container.jsx
@@ -10,7 +10,6 @@ const SwapContainer = () => {
   const [oracleId, setOracleId] = React.useState([]);
   const [balance, setBalance] = React.useState(0);
   const { address } = useAccount();
-
   const address2 = useSelector(state => state.account.account.account);
 
   React.useEffect(() => {
