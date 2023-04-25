7272e0ae5fe76aeb6d8a8431971d5a205f3e2440
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-04-25 20:19:49 +0900

    0425
    0425

--- firstSolar/back/src/routes/swap.ts
+++ firstSolar/back/src/routes/swap.ts
@@ -7,6 +7,12 @@ import { AbiItem } from "web3-utils";
 import { abi as DFSAbi } from "../../contracts/artifacts/Token.json";
 import { abi as DfsEthPoolAbi } from "../../contracts/artifacts/LiquidityPool.json";
 
+import {
+  deployedDFS,
+  deployedETH,
+  deployedUSDT,
+  deployedBNB,
+} from "../deployList/index";
 // MainNet
 const web3 = new Web3(
   "wss://polygon-mumbai.g.alchemy.com/v2/U60psLWRd8tg7yShqQgZ-1YTMSYB0EGo"
@@ -19,7 +25,7 @@ router.use(express.urlencoded({ extended: true }));
 router.post("/swapApprove", async (req, res) => {
   try {
     const filterToken = async () => {
-      switch (req.body.data) {
+      switch (req.body.tokenName) {
         case "dfs":
           return new web3.eth.Contract(DFSAbi as AbiItem[], process.env.DFS);
 
@@ -60,7 +66,7 @@ router.post("/swapApprove", async (req, res) => {
 router.post("/swapTransaction", async (req, res) => {
   try {
     const filterPool = async () => {
-      let target = req.body.data;
+      let target = req.body.poolName;
 
       if ("dfsethpool".includes(target)) {
         return new web3.eth.Contract(
@@ -98,6 +104,33 @@ router.post("/swapTransaction", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.send();
+  }
+});
+
+router.post("/swapBalance", async (req, res) => {
+  try {
+    const myTokenBalance = [];
+    const target = await req.body.userAddress;
+    let balanceDFS = await deployedDFS.methods.balanceOf(target).call();
+    let balanceETH = await deployedETH.methods.balanceOf(target).call();
+    let balanceUSDT = await deployedUSDT.methods.balanceOf(target).call();
+    let balanceBNB = await deployedBNB.methods.balanceOf(target).call();
+
+    const dfs = Math.floor(balanceDFS / 10 ** 18).toString();
+    const eth = Math.floor(balanceETH / 10 ** 18).toString();
+    const usdt = Math.floor(balanceUSDT / 10 ** 18).toString();
+    const bnb = Math.floor(balanceBNB / 10 ** 18).toString();
+
+    myTokenBalance.push({
+      dfs,
+      eth,
+      usdt,
+      bnb,
+    });
+
+    res.send(myTokenBalance);
+  } catch (error) {
+    console.log(error);
   }
 });
 
