f088e4747f37500f1d3a99d9f45a0e0e1982ea77
ChoiWonGyeom <cocomama333@naver.com>
2023-04-25 14:47:38 +0900

    230425

--- firstSolar/front/src/ui-components/Swap768px.jsx
+++ firstSolar/front/src/ui-components/Swap768px.jsx
@@ -30,9 +30,78 @@ export default function Swap768px(props) {
   const dispatch = useDispatch();
 
   const [questionMark, setQuestionMark] = React.useState(0);
-  const isDesktop = useMediaQuery({
-    query: "(min-width:481px)",
-  });
+  const [convertedAmount, setConvertedAmount] = React.useState(0);
+  const [keyWord, setKeyWord] = React.useState(""); //swap 하려는 수량
+  const [secondKeyWord, setSecondKeyWord] = React.useState(""); // swap을 통해 받으려는 토큰정보
+  const [firstSelectTokenPrice, setSelectTokenPrice] = React.useState(0); // swap을 희망하는 토큰정보
+  const [convertPrice, setConvertPrice] = React.useState({
+    bnb: "",
+    eth: "",
+    usdt: "",
+  }); // Convert 토큰들을 filter 해서 해당하는 값을 기입한다.
+  const [secondSelectTokenPrice, setSecondSelectTokenPrice] =
+    React.useState(""); // 어떤 Convert를 할껀지 값을 찾아준다.
+  const [selectedOptions, setSelectedOptions] = React.useState({}); // handle 함수를 호출해서서 현재값을 확인합니다.
+  const [selectedSeconOptions, setSelectedSecondOptions] = React.useState({}); // handleSecondChange 함수를 호출해서 현재값을 확인합니다.
+  const handleChange = e => {
+    setSelectedOptions(e.target.value);
+  };
+  const handleSecondChange = e => {
+    setSelectedSecondOptions(setSecondKeyWord(e.target.value.toLowerCase()));
+  };
+
+  const [swapConvertPrice, setSwapConvertPrice] = React.useState(0); // handle 함수를 호출해서서 현재값을 확인합니다.
+  const [returnSwapConvertPrice, setReturnSwapConvertPrice] = React.useState(0); // swap된 결과값을 나타내 줍니다.
+
+  React.useEffect(() => {
+    setSwapConvertPrice(+keyWord * +firstSelectTokenPrice);
+  }, [selectedOptions, keyWord, secondKeyWord, firstSelectTokenPrice]);
+
+  React.useEffect(() => {
+    setReturnSwapConvertPrice(swapConvertPrice / +secondSelectTokenPrice);
+  }, [
+    swapConvertPrice,
+    secondSelectTokenPrice,
+    secondKeyWord,
+    selectedSeconOptions,
+  ]);
+
+  React.useEffect(
+    () => {
+      (async () => {
+        try {
+          dispatch(isLoadingThunk({ isLoading: true }));
+          const data = await lpBalance(
+            address ? address : address2,
+            firstSelectTokenPrice // 임시로 넣은 것.
+          );
+          setUserBalance(5000); // 임시 값
+          dispatch(isLoadingThunk({ isLoading: false }));
+        } catch (error) {
+          console.error(error);
+          dispatch(isLoadingThunk({ isLoading: false }));
+        }
+      })();
+    },
+    [
+      // 지금은 비워두지만 서로 바꾸는 기능을 만들면 해당 state를 추가할 것.
+    ]
+  );
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const result = await firstSync();
+        if (firstSync) {
+          return;
+        } else {
+          firstSync();
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
   const allowedKeys = [
     "0",
     "1",
@@ -67,7 +136,7 @@ export default function Swap768px(props) {
 
   const setPercentBalance = percentNum => {
     if (props?.balance == 0) return;
-    setSwapMoney(swapMoney * percentNum);
+    setKeyWord(keyWord * percentNum);
   };
 
   const delayedFunction = debounce(num => {
@@ -443,7 +512,7 @@ export default function Swap768px(props) {
                 labelHidden={false}
                 variation="default"
                 onChange={e => {
-                  setSwapMoney(e.target.value);
+                  setKeyWord(e.target.value);
                 }}
                 onKeyPress={e => {
                   handleKeyPress(e);
@@ -482,7 +551,7 @@ export default function Swap768px(props) {
                   position="relative"
                   padding="0px 0px 0px 0px"
                   whiteSpace="pre-wrap"
-                  children={swapMoney ? swapMoney : 0}
+                  children={keyWord ? keyWord : 0}
                   {...getOverrideProps(overrides, "12312312312312312339752827")}
                 ></Text>
                 <Text
@@ -879,7 +948,7 @@ export default function Swap768px(props) {
                 labelHidden={false}
                 variation="default"
                 onChange={e => {
-                  setSwapMoney(e.target.value);
+                  setKeyWord(e.target.value);
                 }}
                 onKeyPress={e => {
                   handleKeyPress(e);
@@ -918,7 +987,7 @@ export default function Swap768px(props) {
                   position="relative"
                   padding="0px 0px 0px 0px"
                   whiteSpace="pre-wrap"
-                  children={swapMoney ? swapMoney : 0}
+                  children={keyWord ? keyWord : 0}
                   {...getOverrideProps(overrides, "12312312312312312339752827")}
                 ></Text>
                 <Text
