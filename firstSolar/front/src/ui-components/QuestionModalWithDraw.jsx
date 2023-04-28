b7ef341f9c9a3e18db2be692956f73474ffdd2f7
ChoiWonGyeom <cocomama333@naver.com>
2023-04-28 12:42:27 +0900

    cwg rebase

--- firstSolar/front/src/ui-components/QuestionModalWithDraw.jsx
+++ firstSolar/front/src/ui-components/QuestionModalWithDraw.jsx
@@ -14,7 +14,7 @@ import "../css/Font.css";
 export default function QuestionModalWithDraw(props) {
   const { overrides, setquestionmark, ...rest } = props;
 
-  console.log("item", props.myPagelist);
+  console.log("item", props.mypagelist);
 
   return (
     <ModalCover
@@ -204,7 +204,7 @@ export default function QuestionModalWithDraw(props) {
                   borderRadius="35px"
                   padding="0px 0px 0px 0px"
                   objectFit="cover"
-                  src={props.myPagelist.mainNetLogo || logo}
+                  src={props.mypagelist.mainNetLogo || logo}
                   {...getOverrideProps(overrides, "ghrgclzzd 740822785")}
                 ></Image>
                 <Text
@@ -227,7 +227,7 @@ export default function QuestionModalWithDraw(props) {
                   position="relative"
                   padding="0px 0px 0px 0px"
                   whiteSpace="pre-wrap"
-                  children={props.myPagelist.firstToken || "불러오는 중"}
+                  children={props.mypagelist.firstToken || "불러오는 중"}
                   {...getOverrideProps(overrides, "DEX Name40822786")}
                 ></Text>
                 <Image
@@ -307,7 +307,7 @@ export default function QuestionModalWithDraw(props) {
                   borderRadius="35px"
                   padding="0px 0px 0px 0px"
                   objectFit="cover"
-                  src={props.myPagelist.platformLogo || logo}
+                  src={props.mypagelist.platformLogo || logo}
                   {...getOverrideProps(overrides, "ghrgclzzd 740822791")}
                 ></Image>
                 <Text
@@ -330,7 +330,7 @@ export default function QuestionModalWithDraw(props) {
                   position="relative"
                   padding="0px 0px 0px 0px"
                   whiteSpace="pre-wrap"
-                  children={props.myPagelist.secondToken || "불러오는 중"}
+                  children={props.mypagelist.secondToken || "불러오는 중"}
                   {...getOverrideProps(overrides, "DEX Name40822792")}
                 ></Text>
                 <Image
@@ -437,7 +437,7 @@ export default function QuestionModalWithDraw(props) {
                     borderRadius="35px"
                     padding="0px 0px 0px 0px"
                     objectFit="cover"
-                    src={props.myPagelist.mainNetLogo || logo}
+                    src={props.mypagelist.mainNetLogo || logo}
                     {...getOverrideProps(overrides, "ghrgclzzd 740822807")}
                   ></Image>
                   <Text
@@ -459,7 +459,7 @@ export default function QuestionModalWithDraw(props) {
                     position="relative"
                     padding="0px 0px 0px 0px"
                     whiteSpace="pre-wrap"
-                    children={props.myPagelist.firstToken || "불러오는 중"}
+                    children={props.mypagelist.firstToken || "불러오는 중"}
                     {...getOverrideProps(overrides, "DEX Name40822808")}
                   ></Text>
                 </Flex>
@@ -497,7 +497,7 @@ export default function QuestionModalWithDraw(props) {
                     whiteSpace="pre-wrap"
                     children={`Balance : ${
                       Math.floor(
-                        props?.myPagelist?.OtherTokenBalance.slice(0, 6)
+                        props?.mypagelist?.OtherTokenBalance.slice(0, 6)
                       ) / 100
                     } `}
                     {...getOverrideProps(overrides, "Balance : 040822813")}
@@ -570,7 +570,7 @@ export default function QuestionModalWithDraw(props) {
                     borderRadius="35px"
                     padding="0px 0px 0px 0px"
                     objectFit="cover"
-                    src={props.myPagelist.platformLogo || logo}
+                    src={props.mypagelist.platformLogo || logo}
                     {...getOverrideProps(overrides, "ghrgclzzd 740822818")}
                   ></Image>
                   <Text
@@ -592,7 +592,7 @@ export default function QuestionModalWithDraw(props) {
                     position="relative"
                     padding="0px 0px 0px 0px"
                     whiteSpace="pre-wrap"
-                    children={props.myPagelist.secondToken || "불러오는 중"}
+                    children={props.mypagelist.secondToken || "불러오는 중"}
                     {...getOverrideProps(overrides, "DEX Name40822819")}
                   ></Text>
                 </Flex>
@@ -630,7 +630,7 @@ export default function QuestionModalWithDraw(props) {
                     whiteSpace="pre-wrap"
                     children={`Balance : ${
                       Math.floor(
-                        props?.myPagelist?.DFSTokenBalance.slice(0, 6)
+                        props?.mypagelist?.DFSTokenBalance.slice(0, 6)
                       ) / 100
                     } `}
                     {...getOverrideProps(overrides, "Balance : 040822824")}
