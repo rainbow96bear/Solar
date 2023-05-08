0549b4af158f0507e702143edd7416348742baa9
KimSungJin <rainbow96bear@gmail.com>
2023-05-08 14:11:19 +0900

    IReward.sol로 통일

--- firstSolar/back/contracts/IReward.sol
+++ firstSolar/back/contracts/IReward.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.19;
 
 interface IReward {
   function rewards() external;
+
+  function autoCompound(
+    address _lpAddress,
+    uint256 _amount
+  ) external returns (uint256);
+
+  function distribution(address _contributor, uint256 _amount) external;
+
+  function sendProfit() external returns (uint256);
 }
