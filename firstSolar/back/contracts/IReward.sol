fbb385c83cf17c4d0f8300a20447e643034ea563
KimSungJin <rainbow96bear@gmail.com>
2023-05-08 13:56:37 +0900

    DFS pool AutoCompounding

--- firstSolar/back/contracts/IReward.sol
+++ firstSolar/back/contracts/IReward.sol
@@ -3,10 +3,4 @@ pragma solidity ^0.8.19;
 
 interface IReward {
   function rewards() external;
-
-  function distribution(address, uint256) external;
-
-  function sendProfit() external returns (uint256);
-
-  function autoCompound(address, uint256) external returns (uint256);
 }
