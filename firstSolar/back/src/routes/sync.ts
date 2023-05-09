825f2b3755a292d1f76180109bbd3e24bdefb9fa
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-05-09 16:04:23 +0900

    NAN, undefiend list값 최신화,검색 결과에 따른 결과값 페이지네이션 수정

--- firstSolar/back/src/routes/sync.ts
+++ firstSolar/back/src/routes/sync.ts
@@ -191,9 +191,8 @@ router.get("/lastDayOfMonth", async (req: Request, res: Response) => {
     const lastDayOfMonth = getLastDayOfMonth(year, month);
 
     if (today == lastDayOfMonth.getDate()) {
-      // airDrop 함수 실행
       const distribution = await deployed.methods.allDistribution().encodeABI();
-      // from: 메타마스크 연결될 사람 to: dexCA
+
       res.send({
         status: 200,
         data: {
