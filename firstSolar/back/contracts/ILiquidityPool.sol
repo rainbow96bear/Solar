8a61f46f5e2b93a1839b0cc3bf955a3890a5a67f
pippen11 <111038332+pippen11@users.noreply.github.com>
2023-05-09 10:18:33 +0900

    230509

---
+++ firstSolar/back/contracts/ILiquidityPool.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface ILiquidityPool {
+  function getReserves()
+    external
+    returns (uint256 _reserve1, uint256 _reserve2);
+
+  function minter(address _to, uint256 _amount) external;
+}
